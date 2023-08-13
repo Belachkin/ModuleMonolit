c77a0c23fbfc6d78d02799ad35856259b13add79
Danila <belkin.job123@gmail.com>
2023-08-13 18:04:56 +0300

    Add Abstract Factory

--- Assets/Source/Game/Scripts/GameRoot.cs
+++ Assets/Source/Game/Scripts/GameRoot.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
 using Movement;
 using Shooting;
+using Enemies;
+
 public class GameRoot : MonoBehaviour
 {
     [SerializeField] private MovementInput _movementInput;
     [SerializeField] private WeaponInput _weaponInput;
+    [SerializeField] private EnemySpawner _enemySpawner;
+
     private void Start()
     {
         _movementInput.Activate();
---
+++ Assets/Source/Modules/Enemies/Scripts/RangeEnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Enemies
+{
+    internal class RangeEnemySpawner : EnemySpawner
+    {
+        [SerializeField] private Enemy _enemy;
+        [SerializeField] private Transform[] _spawnPoints;
+
+        private int index = 0;
+        protected override Enemy Spawn()
+        {
+            Transform spawnPoint = _spawnPoints[index];
+            index++;
+
+            return Instantiate(_enemy, spawnPoint.position, spawnPoint.rotation);
+
+        }
+    }
+}
